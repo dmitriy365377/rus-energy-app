e0ff21b8d636687d928e8def617b24ad66832a12
dmitriy365377 <tigr365377@gmail.com>
2020-09-13 00:00:46 +0300

    add config for firebase

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const app = firebase.initializeApp({
+    apiKey: "AIzaSyBnU24N3nDSKOb590epRbw8q_6gqDUCB8s",
+    authDomain: "fir-react-auth-4915c.firebaseapp.com",
+    databaseURL: "https://fir-react-auth-4915c.firebaseio.com",
+    projectId: "fir-react-auth-4915c",
+    storageBucket: "fir-react-auth-4915c.appspot.com",
+    messagingSenderId: "237490043936",
+    appId: "1:237490043936:web:bb5322e9e45cb9575f5045"
+});
+
+firebase.firestore()
+
+export default firebase;
